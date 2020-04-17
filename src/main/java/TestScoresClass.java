9e9aca81d99f18230f4350012c5fb6b93bd5962f
Tidal <Tidal@192.168.0.109>
2020-04-16 19:37:12 -0500

    added TestScores Class

---
+++ src/main/java/TestScoresClass.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tidal
+ */
+public class TestScoresClass {
+    private double testScores[];
+
+    public TestScoresClass(double[] testScores) {
+        this.testScores = testScores;
+    }
+
+    public double[] getTestScores() {
+        return testScores;
+    }
+
+    public void setTestScores(double[] testScores) throws InvalidTestScoreException {
+      for(int index = 0; index < testScores.length; index++){
+      if(testScores[index] < 0 || testScores[index] >100){
+      throw new InvalidTestScoreException("This array contains test scores that are not valid.");
+      }
+      else
+        this.testScores = testScores;
+      }
+    }
+    public static double getAverage(double[] testScores){
+        double total = 0;
+        for(int index = 0; index < testScores.length; index++){
+            total = total + testScores[index];        
+        
+        }
+      int arrayLength = testScores.length;
+      double average = total / arrayLength;
+      return average;
+    }
+}
