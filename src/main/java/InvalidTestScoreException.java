ae6935877d9691505dd942e0805f61228a4eea48
Tidal <Tidal@192.168.0.109>
2020-04-16 19:36:56 -0500

    added InvalidTestScoreException

---
+++ src/main/java/InvalidTestScoreException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tidal
+ */
+public class InvalidTestScoreException extends Exception {
+    public InvalidTestScoreException(String message){
+    super(message);
+    }
+}
